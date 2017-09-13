8866c0addac08bb28582ece4853e0c90daca9694
Alberto Medrano <albersna@hotmail.com>
2017-09-13 16:26:04 +0100

    Adding subscription with Consumer and not with DisposableObserver...

--- app/src/main/java/development/alberto/com/trakttvtest/data/datarepository/DataTraktTmdbRepository.kt
+++ app/src/main/java/development/alberto/com/trakttvtest/data/datarepository/DataTraktTmdbRepository.kt
@@ -10,6 +10,7 @@ import development.alberto.com.trakttvtest.data.model.tmdb.Tmdb
 import development.alberto.com.trakttvtest.data.service.RetrofitService
 import development.alberto.com.trakttvtest.domain.domainrepository.CallDataTraktTmdbRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.Consumer
 import io.reactivex.observers.DisposableObserver
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.Executors
@@ -27,24 +28,6 @@ class DataTraktTmdbRepository(): CallDataTraktTmdbRepository {
 
 //Mix of Apis in order to make one main one to pass to caching and rest of the app....
     override fun getDataRepositoryMerged() {
-
-        val doTmdb = object : DisposableObserver<Tmdb>() {
-            override fun onComplete() {
-                println("finished y ole!")
-            }
-
-            override fun onNext(tmdb: Tmdb) {
-                println(tmdb.title)
-                println(tmdb.posterPath)
-                println(tmdb.overview)
-                println(tmdb.releaseDate)
-                CacheRealm.putTmdbToRealmCacheImageMovie( TmdbMapperToRealmImageMovieDetails.mapTmdbToRealmImageMovieDetails(tmdb) )
-            }
-
-            override fun onError(e: Throwable) {
-                println(e.message)
-            }
-        }
     //we call the retrofit in each api trakt and tmdb...
     retrofitService.restService(Constants.TRAKT_URL_BASE)
     apiServiceTrakt = retrofitService.restCreateApiServiceTrakt()
@@ -57,7 +40,8 @@ class DataTraktTmdbRepository(): CallDataTraktTmdbRepository {
                     .subscribeOn(Schedulers.io())
                     .observeOn( Schedulers.from(Executors.newCachedThreadPool()))
                     .unsubscribeOn(AndroidSchedulers.mainThread())
-                    .subscribe(doTmdb)
+                    .subscribe(Consumer{ tmdb ->   CacheRealm.putTmdbToRealmCacheImageMovie( TmdbMapperToRealmImageMovieDetails.mapTmdbToRealmImageMovieDetails(tmdb) )},
+                            Consumer { e -> println("Error: " + e.message )  })
 
         }
 
